277ba7a528530fac202b4e2ee05728ab46dac92d
Taylor Ryan <taylor@orbweaver.com>
2020-06-26 22:40:00 -0400

    Initial Commit

---
+++ octoprint_twitchbot/static/js/twitchbot.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-TwitchBot
+ *
+ * Author: Taylor Ryan
+ * License: AGPLv3
+ */
+$(function() {
+    function TwitchbotViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: TwitchbotViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_twitchbot, #tab_plugin_twitchbot, ...
+        elements: [ /* ... */ ]
+    });
+});
